cf5da70a9a0e8fe732011e91449e4065c0366d2b
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 21:18:04 -0600

    Update axe_game.cpp
    Added function to create a window
    
    TIME: 55:25

---
+++ Project-2/axe_game.cpp
@@ -0,0 +1,6 @@
+#include "raylib.h"
+int main(){
+    //Takes 3 inputs, Width, Height, and Title of the window
+    //                  int     int         "String"
+    InitWindow(500, 500, "Nikita's Window");
+}

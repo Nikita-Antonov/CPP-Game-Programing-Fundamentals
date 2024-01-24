efb4e5793c58e4c49fd7d8d92464e9bb7f68f95d
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 21:30:03 -0600

    Update axe_game.cpp
    Working with :
    -Floats
    -Doubles
    -Bools

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -1,8 +1,13 @@
 #include "raylib.h"
 int main(){
 
-    int windowWidth = 350;
+    int windowWidth = 500;
     int windowHeight = 500;
+
+    float root_bear = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
+
     //Takes 3 inputs, Width, Height, and Title of the window
     //                  int     int         "String"
     InitWindow(windowWidth, windowHeight, "Nikita's Window");

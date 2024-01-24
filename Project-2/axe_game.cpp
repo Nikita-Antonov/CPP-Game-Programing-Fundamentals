c49f08faf19cf26526f495b458ab245907baae8a
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 21:41:07 -0600

    Update axe_game.cpp
    Initialization learning

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -5,8 +5,8 @@ int main(){
     int windowHeight = 500;
 
     float root_bear = 1.99;
-    double cheese_burger = 5.99;
-    bool shouldHaveLunch = true;
+    double cheese_burger{5.99}; // <- Brased Initialization WOW!
+    bool shouldHaveLunch{};    //Set to false last i checked 
 
     //Takes 3 inputs, Width, Height, and Title of the window
     //                  int     int         "String"

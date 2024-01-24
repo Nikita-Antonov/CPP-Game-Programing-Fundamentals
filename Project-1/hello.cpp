6f71a2587ffacd15544f6e119d5e6d094975605e
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 20:55:36 -0600

    Update hello.cpp
    Added a multi line comment describing:
    Name on file;
    My Name;
    Function of Program:
    
    Also added small comments about the text output

--- Project-1/hello.cpp
+++ Project-1/hello.cpp
@@ -1,6 +1,17 @@
+/*
+==================================================================================
+::  Name of File: "hello.cpp"
+::  Developer Name: Nikita Antonov
+::  Programs function: The function of this program is to display text to terminal
+==================================================================================
+*/
+
 #include <cstdio>
 int main(){
+    //Prints out my name
     printf("Hello, my name is Nikita Antonov. \n");
+    //Prints out my home town/city
     printf("My home town is the city of Moscow. \n");
+    //I let you know my hobby :)
     printf("My favorite hobby: Programing :) \n");
 }

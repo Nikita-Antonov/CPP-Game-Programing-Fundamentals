2b053090e50fce93367d19fc80a0ced0bbeabc81
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 23:25:27 -0600

    Update axe_game.cpp
    Implemented Movement to the circle!
    
    TIME: 2:04:07

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -1,19 +1,41 @@
 #include "raylib.h"
 int main(){
 
+    //Window Dimentions
     int windowWidth = 500;
     int windowHeight = 500;
 
     //Takes 3 inputs, Width, Height, and Title of the window
-    //                  int     int         "String"
     InitWindow(windowWidth, windowHeight, "Nikita's Window");
 
+    //Circle Coedinates
+    int circle_x = (windowWidth / 2);
+    int circle_y = (windowHeight / 2);
+
+    SetTargetFPS(60);
     while(!WindowShouldClose()){
         BeginDrawing();
+        ClearBackground(WHITE);
 
-        DrawCircle(windowWidth / 2, windowHeight / 2, 25, BLUE);
+        //Game Logic Begins
 
-        ClearBackground(RED);
+        //Draws Circle at the center of the screen
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if(IsKeyDown(KEY_D)){
+            circle_x = circle_x + 10;
+        }
+        if(IsKeyDown(KEY_A)){
+            circle_x = circle_x - 10;
+        }
+        if(IsKeyDown(KEY_W)){
+            circle_y = circle_y - 10;
+        }
+        if(IsKeyDown(KEY_S)){
+            circle_y = circle_y + 10;
+        }
+
+        //Game Logic Ends
         EndDrawing();
     }
 }

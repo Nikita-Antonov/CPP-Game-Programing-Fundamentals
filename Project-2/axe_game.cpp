aa6345436b6b227f6a39547742057bc2b4cfbf33
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 22:02:53 -0600

    Update axe_game.cpp
    LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -4,18 +4,11 @@ int main(){
     int windowWidth = 500;
     int windowHeight = 500;
 
-    float root_bear = 1.99;
-    double cheese_burger{5.99}; // <- Brased Initialization WOW!
-    bool shouldHaveLunch{};    //Set to false last i checked 
-
-    bool equal{4 == 9};     //Will return false
-    bool notEqual{4 != 9};  //Will return false
-    bool less{4 < 9};       //Will return true
-    bool greater{4 > 9};    //Will return false
-    bool lessEqual{4 <= 9}; //Will reurn true
-    bool greaterEqual{4 >= 9};  //Will return false
-
     //Takes 3 inputs, Width, Height, and Title of the window
     //                  int     int         "String"
     InitWindow(windowWidth, windowHeight, "Nikita's Window");
+
+    while(true){
+        
+    }
 }

0a9f61ae86f617a69291a8dfcbffdeaf22fcaeb5
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 20:48:50 -0600

    Update hello.cpp
    First main function
    first print lines
    
    TIME: 38:48

---
+++ Project-1/hello.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+int main(){
+    printf("Hello, my name is Nikita Antonov. \n");
+    printf("My home town is the city of Moscow. \n");
+    printf("My favorite hobby: Programing :) \n");
+}

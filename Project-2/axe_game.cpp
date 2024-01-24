c33ac2cfafc6d8658d5a39cff151e7b68bbd9875
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-23 21:49:33 -0600

    Update axe_game.cpp
    Comparison Operators
    
    TIME : 1:26:55

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -8,6 +8,13 @@ int main(){
     double cheese_burger{5.99}; // <- Brased Initialization WOW!
     bool shouldHaveLunch{};    //Set to false last i checked 
 
+    bool equal{4 == 9};     //Will return false
+    bool notEqual{4 != 9};  //Will return false
+    bool less{4 < 9};       //Will return true
+    bool greater{4 > 9};    //Will return false
+    bool lessEqual{4 <= 9}; //Will reurn true
+    bool greaterEqual{4 >= 9};  //Will return false
+
     //Takes 3 inputs, Width, Height, and Title of the window
     //                  int     int         "String"
     InitWindow(windowWidth, windowHeight, "Nikita's Window");

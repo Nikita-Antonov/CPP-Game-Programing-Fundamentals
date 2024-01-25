34127555bf73ad85e36946f65e07fba58db1765f
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-25 12:43:52 -0600

    Update axe_game.cpp
    Implemented collision of axe and circle.
    
    Game is complete!
    
    Time: 3:00:42

--- Project-2/axe_game.cpp
+++ Project-2/axe_game.cpp
@@ -33,7 +33,11 @@ int main(){
 
     int direction {10};
 
-    bool collision_with_axe {true};
+    bool collision_with_axe = 
+                            (b_axe_y >= u_circle_y) && 
+                            (u_axe_y <= b_circle_y) && 
+                            (l_axe_x <= r_circle_x) && 
+                            (r_axe_x >= l_circle_x);
 
     SetTargetFPS(60);
     while(!WindowShouldClose()){
@@ -46,6 +50,26 @@ int main(){
         }
         else{
             //Game Logic Begins
+            
+            //Update the edges
+            //Circle
+            l_circle_x = circle_x - circle_radius;
+            r_circle_x = circle_x + circle_radius;
+            u_circle_y = circle_y - circle_radius;
+            b_circle_y = circle_y + circle_radius;
+
+            //Axe
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
+
+            //Update collition with axe;
+            collision_with_axe = 
+                            (b_axe_y >= u_circle_y) && 
+                            (u_axe_y <= b_circle_y) && 
+                            (l_axe_x <= r_circle_x) && 
+                            (r_axe_x >= l_circle_x);
 
             //Draws Circle at the center of the screen
             DrawCircle(circle_x, circle_y, circle_radius, BLUE);

00fc6eebead972747e8d87df8384d91f256e9f50
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-26 13:09:45 -0600

    Update dasher.cpp
    -Implemented delta time
    -changed the gravity and velocity to pixels per second
    
    TIME: 4:16:48

--- Project-3-Dapper_Dasher/dasher.cpp
+++ Project-3-Dapper_Dasher/dasher.cpp
@@ -9,8 +9,8 @@ int main(){
     //Initialize the Window
     InitWindow(window_Width, window_Height, "Dapper Dasher");
 
-    //Acceleration due to gravity (pixles per frame, per frame)
-    const int gravity{1};
+    //Acceleration due to gravity (pixles per second, per second)
+    const int gravity{1'000};
 
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
@@ -23,19 +23,18 @@ int main(){
     scarfyPos.x = window_Width/2 - (scarfyRec.width/2);
     scarfyPos.y = window_Height - (scarfyRec.height);
 
-    //Rectangle Dimentions
-    const int width {50};
-    const int height {80};
-
-    int posY {window_Height - height};
     int velocity {0};
 
     bool isInAir {};
-    //jump velovity
-    const int jumpVel {-22};
+    //jump velocity (Pixles Per second)
+    const int jumpVel {-600};
     
     SetTargetFPS(60);
     while(!WindowShouldClose()){
+
+        //Delta time (time since last frame)
+        const float dt {GetFrameTime()};
+
         //Start Drawing
         BeginDrawing();
         //Clears Background
@@ -43,30 +42,35 @@ int main(){
 
 
         //Preform ground check
-        if(posY >= window_Height - height){
+        if(scarfyPos.y >= window_Height - scarfyRec.width){
             //Rectangle is on the ground
             velocity = 0;
             isInAir = false;
         }
         else{
             //Rectangle is in the air
-            velocity += gravity;
+            velocity += gravity * dt;
             isInAir = true;
         }
 
+
+
         //Jump check
-        if(IsKeyPressed(KEY_SPACE) && !isInAir){    //Subtracting to move up, because window is initialized from the top AND checking if we are NOT in the air
-            velocity -= jumpVel;
+        if(IsKeyPressed(KEY_SPACE) && !isInAir){    //Addint the negative jump vel, because window is initialized from the top AND checking if we are NOT in the air
+            velocity += jumpVel;
         }
 
         //Update Position
-        posY += velocity;
+        scarfyPos.y += velocity * dt;
         
-        DrawRectangle(window_Width/2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         //Stop Drawing
         EndDrawing();
     }
+    //Unloads the Scarfy Texture
+    UnloadTexture(scarfy);
+
     CloseWindow();
 
 }

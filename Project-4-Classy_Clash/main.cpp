23a0c0f441120b963ee1f7bcd4b6c6733d1cc92c
Nikita-Antonov <antonovenikita@gmail.com>
2024-01-30 16:43:51 -0600

    Update main.cpp
    Included raymath.h
    
    Added WASD Movement
    
    TIME: 8:29:14

--- Project-4-Classy_Clash/main.cpp
+++ Project-4-Classy_Clash/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main(){
 
@@ -11,6 +12,9 @@ int main(){
 
     //Loading the Map
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos {0.0, 0.0};
+
+    float speed {4.0};
 
     //Main Game Loop
     SetTargetFPS(60);
@@ -20,8 +24,18 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
+        //Creates a direction vector for the charecter movement
+        Vector2 direction {};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+        if(Vector2Length(direction) != 0.0){
+            //Set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         //Draw the map to screen
-        Vector2 mapPos {0.0, 0.0};
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         //End Drawing

90dd6e0748d579e0f0d8b9b774f452cf87f7ab68
Nikita-Antonov <antonovenikita@gmail.com>
2024-02-06 16:53:01 -0600

    Added Headers and Classes
    Added a Character class and header
    
    Condensed the code for better readability

---
+++ Project-4-Classy_Clash/Character.cpp
@@ -0,0 +1,58 @@
+#include "Character.h"
+
+Character::Character(){
+    width = texture.width / maxFrames;
+    height = texture.height;
+}
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{ // Fully qualifying the function (:: <- Scope resolution operator)
+
+    screenPos = {
+        (float)winWidth / 2.0f - (4.0f * (0.5f * width)),
+        (float)winHeight / 2.0f - (4.0f * (0.5f * height))};
+}
+
+void Character::tick(float deltaTime)
+{
+
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // Set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        // Turnary operator, basically an if/else
+        // Condition        if ->               else ->
+        direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
+
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    // Update Animation Frame
+    runningTime += deltaTime; // deltaTime
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.0f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // Draw the Charecter
+    Rectangle source{frame * width, 0.0f, rightLeft * width / 6.0f, height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * width, 4.0f * height};
+
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.0f, WHITE);
+}
--- Project-4-Classy_Clash/main.cpp
+++ Project-4-Classy_Clash/main.cpp
@@ -1,96 +1,44 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "Character.h"
 
-class Charecter{
-public:
+int main()
+{
 
-private:
+    // Setting the window Dimentions
+    const int windowWidth{384};
+    const int windowHeight{384};
 
-};
-
-int main(){
-
-    //Setting the window Dimentions
-    const int windowWidth {384};
-    const int windowHeight {384};
-
-    //Initializing the window
+    // Initializing the window
     InitWindow(windowWidth, windowHeight, "Classy Clash");
 
-    //Loading the Map
+    // Loading the Map
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
-    Vector2 mapPos {0.0, 0.0};
+    Vector2 mapPos{0.0, 0.0};
 
-    //Charecter speed
-    float speed {4.0};
+    Character knight;
+    knight.setScreenPos(windowWidth, windowHeight);
 
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-
-    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
-    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
-
-    Vector2 knightPos {
-        (float)windowWidth/2.0f - (4.0f * (0.5f * (float)knight.width/6.0f)),
-        (float)windowHeight/2.0f - (4.0f * (0.5f * (float)knight.height))
-    };
-    //1: facing right, -1: facing left
-    float rightLeft {1.0f};
-    
-    //Animnation Variables
-    float runningTime {};
-    int frame {};
-    const int maxFrames {6};
-    const float updateTime {1.0f/12.0f};
-
-    //Main Game Loop
+    // Main Game Loop
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        //Start Drawing
+        // Start Drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
-        //Creates a direction vector for the charecter movement
-        Vector2 direction {};
-        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if(IsKeyDown(KEY_D)) direction.x += 1.0;
-        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if(IsKeyDown(KEY_S)) direction.y += 1.0;
-        if(Vector2Length(direction) != 0.0){
-            //Set mapPos = mapPos - direction
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-            //Turnary operator, basically an if/else
-            //Condition        if ->               else ->
-            direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.0f);
 
-            knight = knight_run;
-        }else{
-            knight = knight_idle;
-        }
-
-        //Draw the map to screen
+        // Draw the map to screen
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        //Update Animation Frame
-        runningTime += GetFrameTime(); //deltaTime
-        if(runningTime >= updateTime){
-            frame++;
-            runningTime = 0.0f;
-            if(frame > maxFrames) frame = 0;
-        }
+        knight.tick(GetFrameTime());
 
-        //Draw the Charecter
-        Rectangle source {frame * (float)knight.width/6.0f, 0.0f, rightLeft * (float)knight.width/6.0f, (float)knight.height};
-        Rectangle dest {knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-
-        DrawTexturePro(knight, source, dest, Vector2 {},0.0f, WHITE);
-
-        //End Drawing
+        // End Drawing
         EndDrawing();
     }
-    //Unloads the Map
+    // Unloads the Map
     UnloadTexture(map);
 
     CloseWindow();
-
 }
